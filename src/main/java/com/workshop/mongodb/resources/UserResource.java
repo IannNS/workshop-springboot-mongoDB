53b9f40eb18c792f741289e61e4dda9f47153978
IannNS <iannnschmith@gmail.com>
2026-01-07 17:32:39 -0300

    Creating user post method and DTO integration

--- src/main/java/com/workshop/mongodb/resources/UserResource.java
+++ src/main/java/com/workshop/mongodb/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.workshop.mongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.workshop.mongodb.domain.User;
 import com.workshop.mongodb.dto.UserDTO;
@@ -35,5 +38,15 @@ public class UserResource {
 		
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	// Could also use '@PostMapping'
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		// Get address for the newly created object 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
