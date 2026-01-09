bd873cede29572b98389ee937e2d88d7b0e065ad
IannNS <iannnschmith@gmail.com>
2026-01-09 15:18:09 -0300

    Creating PostDTO and integrating user data to it

--- src/main/java/com/workshop/mongodb/config/Instantiation.java
+++ src/main/java/com/workshop/mongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.workshop.mongodb.domain.Post;
 import com.workshop.mongodb.domain.User;
+import com.workshop.mongodb.dto.AuthorDTO;
 import com.workshop.mongodb.repository.PostRepository;
 import com.workshop.mongodb.repository.UserRepository;
 
@@ -36,10 +37,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
 		User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Going into a trip!", "I am heading to São Paulo today!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good morning", "Woke up happy today!", maria);
-			
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Going into a trip!", "I am heading to São Paulo today!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good morning", "Woke up happy today!", new AuthorDTO(maria));
+			
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	
