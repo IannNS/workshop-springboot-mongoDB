3ce4ac474fae3348155f2fddc9100ede25585173
IannNS <iannnschmith@gmail.com>
2026-01-12 15:08:12 -0300

    Creating Post Comment integration and data alignment

--- src/main/java/com/workshop/mongodb/config/Instantiation.java
+++ src/main/java/com/workshop/mongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.workshop.mongodb.domain.Post;
 import com.workshop.mongodb.domain.User;
 import com.workshop.mongodb.dto.AuthorDTO;
+import com.workshop.mongodb.dto.CommentDTO;
 import com.workshop.mongodb.repository.PostRepository;
 import com.workshop.mongodb.repository.UserRepository;
 
@@ -42,6 +43,13 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Going into a trip!", "I am heading to São Paulo today!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good morning", "Woke up happy today!", new AuthorDTO(maria));
 			
+		CommentDTO c1 = new CommentDTO("Have a nice trip, bro!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Enjoy!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Have a wonderful day!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
