5f31ab18c73e57e03265d340ffeff8ac5b360355
IannNS <iannnschmith@gmail.com>
2026-01-12 15:47:11 -0300

    Creating user request with Query Methods based on title text

--- src/main/java/com/workshop/mongodb/services/PostService.java
+++ src/main/java/com/workshop/mongodb/services/PostService.java
@@ -1,12 +1,12 @@
 package com.workshop.mongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.mongodb.domain.Post;
-import com.workshop.mongodb.domain.User;
 import com.workshop.mongodb.repository.PostRepository;
 import com.workshop.mongodb.services.exception.ObjectNotFoundException;
 
@@ -20,5 +20,9 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id); 
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found")); 
+	}
+	
+	public List<Post> findTitleContaining(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
