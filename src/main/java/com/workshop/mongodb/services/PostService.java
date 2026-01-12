de90394d7ab906691124b6544206ca31a75f00cf
IannNS <iannnschmith@gmail.com>
2026-01-12 14:58:32 -0300

    Creating Post endpoint to return by Id

---
+++ src/main/java/com/workshop/mongodb/services/PostService.java
@@ -0,0 +1,24 @@
+package com.workshop.mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workshop.mongodb.domain.Post;
+import com.workshop.mongodb.domain.User;
+import com.workshop.mongodb.repository.PostRepository;
+import com.workshop.mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) { 
+		Optional<Post> obj = repo.findById(id); 
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found")); 
+	}
+}
